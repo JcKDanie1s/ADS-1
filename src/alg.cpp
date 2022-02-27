c4918ac703dfcf6f78be3aa04de8bcf6452cc2d7
JcKDanie1s <99392811+JcKDanie1s@users.noreply.github.com>
2022-02-27 23:11:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,56 +5,68 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
-if (value == 1 || value == 0)
-{
-return false;
-}
-for (uint64_t i = 2; i <= value / 2; ++i)
-{
-if (value % i == 0)
-{
-return false;
-}
-}
-return true;
+    bool check = true;
+
+    if ((value % 2 == 1) && (value != 1)) {
+        for (int k = 3; k <= sqrt(value); k++) {
+            if (value % k == 0) {
+                check = false;
+                break;
+            }
+        }
+    } else if (value != 2) {
+        check = false;
+    }
+
+    return check;
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
-int j = 0;
-for (uint64_t i = 2;; ++i)
-{
-if (checkPrime(i))
-{
-++j;
-if (j == n)
-{
-return i;
-}
-}
-}
+    uint64_t i = 2;
+
+    if (n != 1) {
+        i = 1;
+        while (n != 1) {
+            i += 2;
+            if (checkPrime(i)) {
+                n--;
+            }
+        }
+    }
+
+    return i;
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
-for (uint64_t i = value + 1;; ++i)
-{
-if (checkPrime(i))
-{
-return i;
-}
-}
+    uint64_t i = value + 1;
+
+    if ((i % 2 == 0) && (i != 2)) {
+        i++;
+    }
+
+    bool check = false;
+    while (!(check)) {
+        if (checkPrime(i)) {
+            check = true;
+            break;
+        }
+        i += 2;
+    }
+
+    return i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
-uint64_t sumP = 0;
-for (uint64_t i = 0; i < hbound; ++i)
-{
-if (checkPrime(i))
-{
-sumP += i;
-}
-}
-return sumP;
+    uint64_t sum = 0;
+
+    for (int i = 1; i < hbound; i++) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
+
+    return sum;
 }
