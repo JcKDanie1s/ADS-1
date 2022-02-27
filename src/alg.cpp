43b8256a9b56e37b4fb4f7e9a8fbf4c9826294b3
JcKDanie1s <99392811+JcKDanie1s@users.noreply.github.com>
2022-02-27 22:58:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,16 +5,56 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
+if (value == 1 || value == 0)
+{
+return false;
+}
+for (uint64_t i = 2; i <= value / 2; ++i)
+{
+if (value % i == 0)
+{
+return false;
+}
+}
+return true;
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
+int j = 0;
+for (uint64_t i = 2;; ++i)
+{
+if (checkPrime(i))
+{
+++j;
+if (j == n)
+{
+return i;
+}
+}
+}
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
+for (uint64_t i = value + 1;; ++i)
+{
+if (checkPrime(i))
+{
+return i;
+}
+}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
+uint64_t sumP = 0;
+for (uint64_t i = 0; i < hbound; ++i)
+{
+if (checkPrime(i))
+{
+sumP += i;
+}
+}
+return sumP;
 }
